da7ec0850b6bb756ec1ba03f59c16b71a55a2d43
David-Horjet <davidojetola956@gmail.com>
2025-11-10 04:18:41 +0100

    chore: modified store

--- store/store.ts
+++ store/store.ts
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "./slices/authSlice"
 import resourcesReducer from "./slices/resourcesSlice"
 import mentorsReducer from "./slices/mentorsSlice"
+import subscriptionsReducer from "./slices/subscriptionsSlice"
+import pointsReducer from "./slices/pointsSlice"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     resources: resourcesReducer,
     mentors: mentorsReducer,
+    subscriptions: subscriptionsReducer,
+    points: pointsReducer,
   },
 })
 
