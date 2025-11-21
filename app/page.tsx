f832f67f6263bbc1f6034175a001edaaedbc1d9d
David-Horjet <davidojetola956@gmail.com>
2025-11-21 16:32:03 +0100

    chore: removed newsletter

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       </section>
       <FeaturesSection />
       <CtaSection />
-      <NewsletterSection />
+      {/* <NewsletterSection /> */}
       <Footer />
     </div>
   )
